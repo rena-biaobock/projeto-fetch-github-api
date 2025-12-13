65256354ff947fac0d403253baf37ac1ca601dcb
rena-biaobock <rena.biaobock.costa@gmail.com>
2025-12-13 16:55:01 -0300

    :sparkles: feat: set getRepositories to return 10 max itens instead off all itens

--- src/scripts/services/repositories.js
+++ src/scripts/services/repositories.js
@@ -1,8 +1,11 @@
 import { baseUrl } from "../variables.js";
 
 async function getRepositories(userName) {
-    const response = await fetch(`${baseUrl}/${userName}/repos`)
-    return await response.json()
+  const maxItems = 10;
+  const response = await fetch(
+    `${baseUrl}/${userName}/repos?per_page=${maxItems}`
+  );
+  return await response.json();
 }
 
-export { getRepositories }
+export { getRepositories };
